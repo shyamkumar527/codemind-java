8136cb86a40d02be6eca301a6e797aab0812fdeb
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-09-09 11:49:35 +0530

    import java.util.*; public class file{     public static void main(String[] args){         String s;         Scanner sc=new Scanner(System.in);         s=sc.nextLine();         System.out.println(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class file{
+    public static void main(String[] args){
+        String s;
+        Scanner sc=new Scanner(System.in);
+        s=sc.nextLine();
+        System.out.println(s);
+    }
+}
