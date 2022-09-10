ecca1904a453081ac3de489ee606b6f27d9440cc
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-09-10 13:16:14 +0530

    import java.util.*; class file{     public static void main(String[] args){         int n,i,k,c=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         k=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++){             arr[i]=sc.nextInt();         }         for(i=0;i<n;i++){             if(arr[i]%k==0){                 c+=1;             }         }         System.out.println(c);     } }

---
+++ Count_elements_divisible_by_k.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class file{
+    public static void main(String[] args){
+        int n,i,k,c=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        k=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++){
+            if(arr[i]%k==0){
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
