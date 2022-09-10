ff09a49655ff9f5fa2ce38320c66df46a450f2d6
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-09-10 14:57:29 +0530

    import java.util.*; public class file{     public static void main(String[] args){         int n,r,ans=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         while(n!=0){             r=n%10;             if(r>ans){                 ans=r;             }             n=n/10;         }         System.out.println(ans);     } }

---
+++ Largest_Digit.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class file{
+    public static void main(String[] args){
+        int n,r,ans=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        while(n!=0){
+            r=n%10;
+            if(r>ans){
+                ans=r;
+            }
+            n=n/10;
+        }
+        System.out.println(ans);
+    }
+}
