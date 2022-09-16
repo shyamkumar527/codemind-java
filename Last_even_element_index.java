845944bf9495adb722b07a1702c2c3343ae9aeb8
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-09-16 13:22:44 +0530

    import java.util.*; public class file{     public static void main(String[] args){         int n,i,ans=-1;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++){             arr[i]=sc.nextInt();             if(arr[i]%2==0){                 ans=i;             }         }         System.out.println(ans);     } }

---
+++ Last_even_element_index.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class file{
+    public static void main(String[] args){
+        int n,i,ans=-1;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+            if(arr[i]%2==0){
+                ans=i;
+            }
+        }
+        System.out.println(ans);
+    }
+}
