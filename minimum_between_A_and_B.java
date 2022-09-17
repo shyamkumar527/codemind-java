819a78972c2ced81314b33a70ad40d996c4e6567
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-09-17 17:21:28 +0530

    import java.util.*; public class file{     public static void main(String[] args){         int n,a,b,i,min=1000,ans=0;         Scanner sc=new Scanner(System.in);         n=sc.nextInt();         int arr[]=new int[n];         for(i=0;i<n;i++){             arr[i]=sc.nextInt();         }         a=sc.nextInt();         b=sc.nextInt();         for(i=0;i<n;i++){             if(arr[i]>=a && arr[i]<=b && arr[i]<min){                 min=arr[i];                 ans=1;             }         }         if(ans==0){             System.out.println("-1");         }         else{             System.out.println(min);         }     } }

---
+++ minimum_between_A_and_B.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class file{
+    public static void main(String[] args){
+        int n,a,b,i,min=1000,ans=0;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        a=sc.nextInt();
+        b=sc.nextInt();
+        for(i=0;i<n;i++){
+            if(arr[i]>=a && arr[i]<=b && arr[i]<min){
+                min=arr[i];
+                ans=1;
+            }
+        }
+        if(ans==0){
+            System.out.println("-1");
+        }
+        else{
+            System.out.println(min);
+        }
+    }
+}
