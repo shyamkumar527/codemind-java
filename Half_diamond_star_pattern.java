77a275b2e23bdd69aab733e2f97d5faf6a66c5a5
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-10-26 22:35:16 +0530

    import java.util.*; public class file{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int i,j;         int n=sc.nextInt();         if(n<=2){             System.out.println("The pattern is not possible");
            }
            else{
                for( i=1;i<n;i++){
            		for( j=0;j<i;j++){
            			System.out.print("*");
            		}
            		System.out.println();
            	}
            	for(i=0;i<n;i++){
            		for(j=n-1;j>=i;j--){
            			System.out.print("*");
            		}
            		System.out.println();
            	}
            }
        }
    }

---
+++ Half_diamond_star_pattern.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class file{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int i,j;
+        int n=sc.nextInt();
+        if(n<=2){
+            System.out.println("The pattern is not possible");
+            
+        }
+        else{
+            for( i=1;i<n;i++){
+        		for( j=0;j<i;j++){
+        			System.out.print("*");
+        		}
+        		System.out.println();
+        	}
+        	for(i=0;i<n;i++){
+        		for(j=n-1;j>=i;j--){
+        			System.out.print("*");
+        		}
+        		System.out.println();
+        	}
+        }
+    }
+}
