dfba59400e460f47e4161975e3470d34719a8ee2
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-12-14 18:37:11 +0530

    import java.util.*; class file{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         System.out.print(s.toLowerCase());     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class file{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        System.out.print(s.toLowerCase());
+    }
+}
