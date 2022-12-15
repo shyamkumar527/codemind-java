0c71f178fd32094949e9ce6c023bcda5079c3d90
shyam kumar <97221940+shyamkumar527@users.noreply.github.com>
2022-12-15 14:49:28 +0530

    import java.util.*; class file{     public static void main(String[] args){         Scanner sc=new Scanner(System.in);         String s=sc.next();         String t=sc.next();         long x=Long.parseLong(s);         long y=Long.parseLong(t);         long l=x*y;         System.out.print(l);     } }

---
+++ Multiply_Strings.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class file{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.next();
+        String t=sc.next();
+        long x=Long.parseLong(s);
+        long y=Long.parseLong(t);
+        long l=x*y;
+        System.out.print(l);
+    }
+}
